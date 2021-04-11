e9e21adf877673096d4fcb99988506e29bcb45cd
moquito64 <moquito64@gmail.com>
2021-04-11 12:07:55 -1000

    works

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { indexPage, messagesPage, addMessage } from '../controllers';
-
+import { modifyMessage, performAsyncAction } from '../middleware';
 const indexRouter = express.Router();
 
 indexRouter.post('/messages', addMessage);
+indexRouter.post('/messages', modifyMessage, performAsyncAction, addMessage);
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', messagesPage);
 
